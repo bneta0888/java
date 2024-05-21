71ccddb59aceaeee97af4a777684eb3a830cc5ef
bneta0888 <binitashrestha0888@gmail.com>
2024-05-21 08:25:04 +0545

    Studentt

---
+++ Studentt.java
@@ -0,0 +1,20 @@
+public class Studentt {
+
+    private String name;
+    private int age;
+    private int rollNo;
+    
+
+    public Studentt(String name, int age, int rollNo ){
+        this.name = name;
+        this.age = age;
+        this.rollNo = rollNo;
+    }
+
+    public static void main(String[] args) {
+        Studentt student1 = new Studentt("Bneta", 19, 01);
+        System.out.printf(" Name : %s\n age : %d\n RollNo : 01", student1.name,student1.age,student1.rollNo );
+        
+    }
+    
+}
